545967e1ce36a014857ccb64b10e367aa8b6f964
우원 <aepeul@aepeul-ui-MacBookPro.local>
2025-05-30 00:59:06 +0900

    refactor: playground page added

---
+++ app/playground/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+const Playground = () => {
+  const goBack = () => {
+    if (window?.webkit?.messageHandlers?.iosNative) {
+      window.webkit.messageHandlers.iosNative.postMessage("back");
+    } else {
+      console.log("하이브리드앱이 아닙니다. 뒤로가기 합니다.");
+      // 일반 웹에서는 history.back()을 사용하여 뒤로가기
+      window.history.back();
+    }
+  };
+
+  return (
+    <div style={{ padding: 40 }} className="flex flex-col items-center">
+      <h1>여기는 플레이그라운드입니다~</h1>
+      <button
+        className="bg-gray-400 rounded-md p-4 mt-4 hover:bg-gray-500 cursor-pointer"
+        onClick={goBack}
+      >
+        뒤로가기 (via Native)
+      </button>
+    </div>
+  );
+};
+
+export default Playground;
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,18 @@
 "use client";
 
 const Profile = () => {
+  const goToPlayground = () => {
+    if (window?.webkit?.messageHandlers?.iosNative) {
+      window.webkit.messageHandlers.iosNative.postMessage(
+        "navigate||/playground"
+      );
+    } else {
+      console.log("하이브리드앱이 아닙니다. 뒤로가기 합니다.");
+      // 일반 웹에서는 history.back()을 사용하여 뒤로가기
+      window.history.back();
+    }
+  };
+
   const goBack = () => {
     if (window?.webkit?.messageHandlers?.iosNative) {
       window.webkit.messageHandlers.iosNative.postMessage("back");
@@ -16,9 +28,15 @@ const Profile = () => {
       <h1>여기는 프로필 보는 곳입니다~</h1>
       <button
         className="bg-gray-400 rounded-md p-4 mt-4 hover:bg-gray-500 cursor-pointer"
+        onClick={goToPlayground}
+      >
+        플레이 그라운드 가기 (via Native)
+      </button>
+      <button
+        className="bg-gray-400 rounded-md p-4 mt-4 hover:bg-gray-500 cursor-pointer"
         onClick={goBack}
       >
-        뒤로가기 (via Native)
+        뒤로 가기 (via Native)
       </button>
     </div>
   );
